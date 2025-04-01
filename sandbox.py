aa6a56df8d3d3589a377c4d7c9b027926db563b4
StewartNash <Stewart.M.Nash@gmail.com>
2025-04-01 09:53:49 -0700

    Fixed bugs and printed initial dataframe

--- sandbox.py
+++ sandbox.py
@@ -32,21 +32,26 @@ OUTPUT_COLUMNS = [
     'explosion'
 ]
 
-#def generate_row(length):
-#    return np.random.rand(length)
-
 def generate_coefficients(input_length, output_length):
-    return np.random.rand(output_length, input_length)
+    mean = 0
+    stddev = 5.0
+    rows = input_length
+    columns = output_length
+    return np.random.normal(mean, stddev, size=(rows, columns))
 
-#def generate_row(input_length, output_length):
 def generate_row(coefficients):
     rows, columns = coefficients.shape
     input_row = np.random.rand(columns)
-    output = np.matmul(coefficents, input_row)
+    output = np.matmul(coefficients, input_row)
+    output = output.tolist()
+    assert len(output) >= 4
+    output[-2] = "leaking" if output[-4] > 0 else "not leaking"
+    output[-1] = "explosion" if output[-3] > 0 else "no explosion"
+    output = input_row.tolist() + output
     return output
 
-my_dataframe = pd.Dataframe(columns=COLUMNS)
+my_dataframe = pd.DataFrame(columns=COLUMNS)
 coefficients = generate_coefficients(len(INPUT_COLUMNS), len(OUTPUT_COLUMNS))
 for i in range(TOTAL_ROWS):
-    #my_dataframe.loc[len(my_dataframe)] = generate_row(len(my_dataframe.columns))
     my_dataframe.loc[len(my_dataframe)] = generate_row(coefficients)
+print(my_dataframe.head())
