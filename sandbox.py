c2f111d08dd5732ac9488825c3d6d27c0e539356
StewartNash <Stewart.M.Nash@gmail.com>
2025-04-06 03:07:29 -0700

    Added code to sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 
 TOTAL_ROWS = 10000
 
@@ -53,6 +55,15 @@ def generate_row(coefficients):
     output = input_row.tolist() + output
     return output
 
+def generate_sets(myset, output_columns, categorical_labels):
+    myset_labels = myset[output_columns].copy()
+    myset = myset.drop(output_columns, axis=1)
+    myset_categorical = myset_labels[categorical_labels]
+    categorical_encoder = OneHotEncoder()
+    myset_categorical = categorical_encoder.fit_transform(myset_categorical)
+    myset_categorical = myset_categorical.toarray()
+    return myset, myset_labels, myset_categorical, categorical_encoder
+
 my_dataframe = pd.DataFrame(columns=COLUMNS)
 coefficients = generate_coefficients(len(INPUT_COLUMNS), len(OUTPUT_COLUMNS))
 for i in range(TOTAL_ROWS):
@@ -60,26 +71,58 @@ for i in range(TOTAL_ROWS):
 print(my_dataframe.head())
 
 welding_train, welding_test = train_test_split(my_dataframe, test_size=0.2, random_state=42)
+welding_train, welding_validation = train_test_split(welding_train)
+
 welding_train_labels = welding_train[OUTPUT_COLUMNS].copy()
-welding_test_labels = welding_test[OUTPUT_COLUMNS].copy()
 welding_train = welding_train.drop(OUTPUT_COLUMNS, axis=1)
+welding_train_categorical = welding_train_labels[CATEGORICAL_LABELS]
+train_categorical_encoder = OneHotEncoder()
+welding_train_categorical = train_categorical_encoder.fit_transform(welding_train_categorical)
+welding_train_categorical = welding_train_categorical.toarray()
+
+welding_test_labels = welding_test[OUTPUT_COLUMNS].copy()
 welding_test = welding_test.drop(OUTPUT_COLUMNS, axis=1)
-welding_categorical = welding_labels[CATEGORICAL_LABELS]
-welding_labels = welding_labels.drop(CATEGORICAL_LABELS, axis=1)
-print(welding_categorical.head())
+welding_test_categorical = welding_test_labels[CATEGORICAL_LABELS]
+test_categorical_encoder = OneHotEncoder()
+welding_test_categorical = test_categorical_encoder.fit_transform(welding_test_categorical)
+welding_test_categorical = welding_test_categorical.toarray()
+
+welding_validation_labels = welding_validation[OUTPUT_COLUMNS].copy()
+welding_validation = welding_validation.drop(OUTPUT_COLUMNS, axis=1)
+welding_validation_categorical = welding_validation_labels[CATEGORICAL_LABELS]
+validation_categorical_encoder = OneHotEncoder()
+welding_validation_categorical = validation_categorical_encoder.fit_transform(welding_validation_categorical)
+welding_validation_categorical = welding_validation_categorical.toarray()
+
+scaler = StandardScaler()
+welding_train = scaler.fit_transform(welding_train)
+welding_test = scaler.transform(welding_test)
+welding_validation = scaler.transform(welding_validation)
+
+print(welding_train_categorical[:10])
+print(train_categorical_encoder.categories_)
 
 import tensorflow as tf
 from tensorflow import keras
 
+print(tf.__file__)
+print(tf.__version__)
+
 INPUT_WIDTH = len(INPUT_COLUMNS)
 OUTPUT_WIDTH = len(OUTPUT_COLUMNS)
+numerical_output_width = welding_train_labels.shape[1]
+categorical_output_width = welding_train_categorical.shape[1]
 
 model = keras.models.Sequential()
 model.add(keras.layers.InputLayer(input_shape=(INPUT_WIDTH, 1)))
 model.add(keras.layers.Dense(INPUT_WIDTH * 10, activation="relu"))
 model.add(keras.layers.Dense(INPUT_WIDTH * 5, activation="relu"))
 model.add(keras.layers.Dense(INPUT_WIDTH * 1, activation="relu"))
-model.add(keras.layers.Dense(OUTPUT_WIDTH, activation="softmax"))
+model.add(keras.layers.Dense(numerical_output_width, activation="softmax"))
 model.compile(loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
-model.fit(X_train, y_train, epochs=30, validation_data=(X_valid, y_valid))
+history = model.fit(welding_train,
+    welding_train_labels,
+    epochs=30,
+    validation_data=(welding_validation, welding_validation_labels))
+mse_test = model.evaluate(welding_test, welding_test_labels)
 
